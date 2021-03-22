0b659798a2d5d917289210a35bab5dafbfd62ecc
Rawan199812 <rawan.13579@icloud.com>
2021-03-23 00:21:01 +0200

    finished

--- js/app.js
+++ js/app.js
@@ -1,38 +1,51 @@
-$(document).ready(function () {
-    const ajaxSettings = {
-      method: 'get',
-      dataType: 'json'
-    };
+let keywordAr = [];
+function Horns(item) {
+    this.image_url = item.image_url;
+    this.title = item.title;
+    this.description = item.description;
+    this.keyword = item.keyword;
+    this.horns = item.horns;
+    Horns.allArr.push(this)
+}
+Horns.allArr = [];
 
-$.ajax('../data/page-1.json', ajaxSettings)
-.then(data => {
-  data.forEach(item => {
-      $('ul').append(`<li><img src=${item.image_url}></li>`);
-      if (item.keyword.includes()) {
-          
-          $('select').append(`<option>${item.keyword}</option>`);
-        }
-    });
-      
-});
-});
-// $('select').on('click', 'option', function () {
-    $('select').on('click', function() {
-    const newArr=[];
+Horns.prototype.renderList = function () {
+    $('ul').append(`<li><img src=${this.image_url}></li><div>${this.title}</div><p>${this.description}</p>
     
-    let selectedValue = $(this).val();
-    console.log('hi');
-    newArr.forEach(element=>{
-        if (element.keyword===selectedValue) {
-            newArr.push(element);
-            console.log(newArr);
-        }
-    });
-    for (let i = 0; i < newArr.length; i++) {
-       
-        $('ul').append(`<li><img src=${newArr[i].image_url}></li>`);
+    `);
+
+};
+Horns.prototype.renderOptions = function () {
+    if (keywordAr.includes(this.keyword)) {
+
+    }else{
+        $('select').append(`<option>${this.keyword}</option>`);
+        keywordAr.push(this.keyword);
         
     }
-
-    
+}
+$(document).ready(function () {
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+};
+$.ajax('../data/page-1.json', ajaxSettings)
+ .then(data => {
+    data.forEach(horns => {
+    let item = new Horns(horns);
+    item.renderList();
+    item.renderOptions();
+  });
 });
+$('select').on('click', function () {
+    let selectedValue = $(this).val();
+    $('ul').empty();
+    for (let i = 0; i < Horns.allArr.length; i++) {
+            if (Horns.allArr[i].keyword === selectedValue) {
+                Horns.allArr[i].renderList();
+            }
+        }
+
+    });
+});
+
