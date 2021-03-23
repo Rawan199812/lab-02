497985add31c82371600269380455f9ce4f4af9b
Rawan199812 <rawan.13579@icloud.com>
2021-03-23 20:20:46 +0200

    not finish

--- js/app.js
+++ js/app.js
@@ -29,15 +29,25 @@ $(document).ready(function () {
         method: 'get',
         dataType: 'json'
 };
-$.ajax('../data/page-1.json', ajaxSettings)
+Horns.prototype.readJson=function(pageNum) {
+    
+
+// const readJson = (pageNum) => {
+    console.log(pageNum);
+$.ajax(`../data/page-${pageNum}.json`, ajaxSettings)
  .then(data => {
     data.forEach(horns => {
     let item = new Horns(horns);
     item.renderList();
     item.renderOptions();
+    
+    
+    
   });
-});
-$('select').on('click', function () {
+
+});}
+Horns.prototype.readJson(1);
+$('select').on('change', function () {
     let selectedValue = $(this).val();
     $('ul').empty();
     for (let i = 0; i < Horns.allArr.length; i++) {
@@ -48,4 +58,46 @@ $('select').on('click', function () {
 
     });
 });
+
+// lab03
+// event for page1 button
+$('#page1').on('click', function (event) {
+    event.preventDefault();
+    console.log('hi');
+    $('select').empty();
+    Horns.allArr.forEach(item=>{
+        $('ul').empty();
+        item.readJson(1);
+        item.renderList();
+        item.renderOptions();
+        
+
+    })
+
+    // empty the option to render it $('select').empty();
+    // Horns.prototype.readJson(1);
+    // item.renderList();
+    // item.renderOptions();
+
+})
+//event for page2 button
+$('#page2').on('click', function (event) {
+    event.preventDefault();
+    console.log('hello');
+
+    //call the render for page2 renderPage2();
+    $('select').empty();
+    // $('ul').hide();
+
+    Horns.allArr.forEach(item=>{
+        
+        $('ul').empty();
+        item.readJson(2);
+        item.renderList();
+        item.renderOptions();
+        
+    })
+    //call the options2 renderOptions2();
+
+})
 
